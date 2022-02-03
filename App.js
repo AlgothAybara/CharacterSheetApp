93aa730d1acd0539f6361a475516d6edde1e9977
ay0900356@byui.edu <64487231+AlgothAybara@users.noreply.github.com>
2022-02-03 09:17:07 -0700

    Uploaded correct App.js

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import ListScreen from './Screens/List_Screen';
+import EditScreen from './Screens/Edit_Screen';
+import NewScreen from './Screens/New_Screen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      
+      <Stack.Navigator initialRouteName="List">
+        <Stack.Screen name="List" component={ListScreen} />
+        <Stack.Screen name="Edit" component={EditScreen} />
+        <Stack.Screen name="New" component={NewScreen} />
+      </Stack.Navigator>
+    
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
