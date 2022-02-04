2482e41427c6aba8146ffa525540cd637f87edcf
AlgothAybara <junioryoung916@gmail.com>
2022-02-04 07:43:25 -0700

    Trying to use the character class object

--- Components/Character_Class.js
+++ Components/Character_Class.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
-class Character {
-    constructor(name, type, attack, defense, body, mind, gold, inventory) {
-      this.name = name;
-      this.type = type;
-      this.attack = attack;
-      this.defense = defense;
-      this.body = body;
-      this.mind = mind;
-      this.gold = gold;
-      this.inventory = inventory;
-    }
+class Character extends React.Component {
+  state = {}
+  
+  constructor(name, type, attack, defense, body, mind, gold, inventory) {
+    this.name = name;
+    this.type = type;
+    this.attack = attack;
+    this.defense = defense;
+    this.body = body;
+    this.mind = mind;
+    this.gold = gold;
+    this.inventory = inventory;
   }
+}
 
-  export default Character;
+export default Character;
