309280a76727466656b88fde9e3e1e084144c98c
ay0900356@byui.edu <64487231+AlgothAybara@users.noreply.github.com>
2022-11-11 00:46:14 -0700

    Replaced exported function with class extending React. Added all form fields and track data with state. Enabled Reset Form and Save buttons.

--- Screens/Edit_Screen.js
+++ Screens/Edit_Screen.js
@@ -1,246 +1,327 @@
 import { Text, Pressable, SafeAreaView, View, Alert, ScrollView, TextInput, Button } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import styles from '../StyleSheets/Edit_Screen.style.js';
-import { useEffect, useState } from 'react'
-import Card from '../Components/Character_Card'
-//import UpDown from '../Components/Item_UpDown'
+import styles from '../StyleSheets/New_Screen.style.js';
+import React, { useState } from 'react';
 
-//Deletes Data and returns to list screen
-async function deleteHandle(key, navigation) {
-  await AsyncStorage.removeItem(key)
-  navigation.goBack()
-}
 
-async function getHandle(key, setItem) {
-
-  //Gets character data from key
-  try {
-    gotten =  await AsyncStorage.getItem(key)
-    } catch(e) {
-      console.warn(e)
+class EditScreen extends React.Component {
+    //Stores variables in local class
+    constructor(props) {
+        super(props);
+        this.state = {
+            key: (temp = this.props.route.params),
+            name: "",
+            clss: "",
+            attk: "",
+            defn: "",
+            body: "",
+            mind: "",
+            gold: "",
+            invt: "",
+        };
+        
+        this.textInput = React.createRef();
+        this.heroChange = this.heroChange.bind(this);
+        this.setData = this.setData.bind(this);
     }
-    
-    //parses character json to dictionary
-    temp = gotten != null ? JSON.parse(gotten) : null
 
-    //sets state hook to parsed data
-    setItem(temp)
-}
-
-async function saveHandle(key, item, navigation) {
-  const jsonValue = JSON.stringify(item)
-
-  try {
-    gotten =  await AsyncStorage.setItem(key, jsonValue)
-    } catch(e) {
-      console.warn(e)
+    //allows update of variables
+    heroChange(name) {
+        return (text) => {
+            this.setState({ [name]:text})
+        }
     }
-  
-    navigation.goBack()
+
+    //Deletes Data and returns to list screen
+    async deleteHandle(navigation) {
+        await AsyncStorage.removeItem(this.state.key)
+        navigation.goBack()
+    }
+
+    componentDidMount() {
+        // Gets character data from key
+        this.loadData(this.state.key.key)
+    }
+
+    async loadData(key) {
+        try {
+            gotten = await AsyncStorage.getItem(key)
+        } catch (e) {
+            console.warn(e)
+        }
+
+        // //parses character json to dictionary
+        temp = gotten != null ? JSON.parse(gotten) : null
+
+        //sets state to parsed data
+
+        this.state.key = temp.key
+        this.state.name = temp.name
+        this.state.clss = temp.clss
+        this.state.attk = temp.attk
+        this.state.defn = temp.defn
+        this.state.body = temp.body
+        this.state.mind = temp.mind
+        this.state.gold = temp.gold
+        this.state.invt = temp.invt
+
+        // console.log(this.state)
+        this.forceUpdate()
+    }
+
+    async setData(navigation) {
+        const jsonValue = JSON.stringify(this.state)
+
+        try {
+            gotten = await AsyncStorage.setItem(this.state.key, jsonValue)
+        } catch (e) {
+            console.warn(e)
+        }
+
+        navigation.goBack()
+    }
+
+    updateRefs(){
+        refList = ["name", "clss", "attk", "defn", "body", "mind", "gold", "invt" ]
+        refList.forEach(ref => {
+            this[ref].va
+        });
+    }
+
+    //Displays alert on the user screen
+    createTwoButtonAlert(navigation) {
+        Alert.alert(
+            "Warning!",
+            "You are about to delete this character. Do you want to continue with this deletion?",
+            [
+                //Cancels deletion
+                {
+                    text: "No",
+                    onPress: () => console.log("Cancel Pressed"),
+                    style: "cancel"
+                },
+                //Deletes data
+                {
+                    text: "Yes",
+                    onPress: () => this.deleteHandle(navigation)
+                }
+            ]
+        );
+    }
+
+    render () {
+        const { navigation } = this.props
+        const {item} = this.state 
+
+        return (
+            // Placeholder screen
+            <SafeAreaView>
+                <ScrollView>
+                {/* Name Header */}
+                <TextInput 
+                    style={styles.header}
+                    onChangeText={this.heroChange("name")}
+                >
+                    {this.state.name}
+                </TextInput>
+                {/* Class SubHeader */}
+                <TextInput 
+                    style={styles.subheader}
+                    onChangeText={this.heroChange("clss")}
+                >
+                    {this.state.clss}
+                </TextInput>
+            
+                {/* UpDown Row */}
+                <View style={styles.row}>
+                    {/* Attack UpDown */}
+                    <View style={styles.box}>
+                        <Text>Attack</Text>
+                        <Button
+                            onPress={() => {
+                                temp = this.state
+                                temp.attk = String(parseInt(temp.attk) + 1)
+                                this.state.attk = temp.attk
+                                this.forceUpdate()
+                            }}
+                            title="+"
+                        />
+                        <TextInput
+                            keyboardType='numeric'
+                        >
+                            {this.state.attk}
+                        </TextInput>
+                        <Button
+                            onPress={
+                                () => {
+                                    if(this.state.attk > 1){
+                                        temp = this.state
+                                        temp.attk = String(parseInt(temp.attk) - 1)
+                                        this.state.attk = temp.attk
+                                        this.forceUpdate()
+                                    }
+
+                                }
+                            }
+                            title="-"
+                        />
+        
+                    </View>
+                    {/* Defense UpDown */}
+                    <View style={styles.box}>
+                        <Text>Defense</Text>
+                        <Button
+                            onPress={() => {
+                                temp = this.state
+                                temp.defn = String(parseInt(temp.defn) + 1)
+                                this.state.defn = temp.defn
+                                this.forceUpdate()
+                            }}
+                            title="+"
+                        />
+                        <TextInput
+                            keyboardType='numeric'
+                        >
+                            {this.state.defn}
+                        </TextInput>
+                        <Button
+                            onPress={
+                                () => {
+                                    if(this.state.defn > 2){
+                                        temp = this.state
+                                        temp.defn = String(parseInt(temp.defn) - 1)
+                                        this.state.defn = temp.defn
+                                        this.forceUpdate()
+                                    }
+                                }
+                            }
+                            title="-"
+                        />
+                    </View>
+                    {/* Body UpDown */}
+                    <View style={styles.box}>
+                        <Text>Body</Text>
+                        <Button
+                            onPress={() => {
+                                temp = this.state
+                                temp.body = String(parseInt(temp.body) + 1)
+                                this.state.body = temp.body
+                                this.forceUpdate()
+                            }}
+                            title="+"
+                        />
+                        <TextInput
+                            keyboardType='numeric'
+                        >
+                            {this.state.body}
+                        </TextInput>
+                        <Button
+                            onPress={
+                                () => {
+                                    if(this.state.body > 0){
+                                        temp = this.state
+                                        temp.body = String(parseInt(temp.body) - 1)
+                                        this.state.body = temp.body
+                                        this.forceUpdate()
+                                    }
+                                }
+                            }
+                            title="-"
+                        />
+                    </View>
+                    {/* Mind UpDown */}
+                    <View style={styles.box}>
+                        <Text>Mind</Text>
+                        <Button
+                            onPress={() => {
+                                temp = this.state
+                                temp.mind = String(parseInt(temp.mind) + 1)
+                                this.state.mind = temp.mind
+                                this.forceUpdate()
+                            }}
+                            title="+"
+                        />
+                        <TextInput
+                            keyboardType='numeric'
+                        >
+                            {this.state.mind}
+                        </TextInput>
+                        <Button
+                            onPress={
+                                () => {
+                                    if(this.state.mind > 0){
+                                        temp = this.state
+                                        temp.mind = String(parseInt(temp.mind) - 1)
+                                        this.state.mind = temp.mind
+                                        this.forceUpdate()
+                                        console.log(this.state)
+                                    }
+                                }
+                            }
+                            title="-"
+                        />
+                    </View>
+                </View>
+                {/* Gold Row */}
+                <View style={styles.form_row}>
+                    <Text style={styles.label}>Gold</Text>
+                    <TextInput
+                        style={styles.input}
+                        onChangeText={this.heroChange("gold")}
+                        keyboardType='numeric'
+                    >
+                        {this.state.gold}
+                    </TextInput>
+                </View>
+                {/* Inventory Row */}
+                <View style={styles.form_row}>
+                    <Text style={styles.subheader}>Inventory</Text>
+                    <TextInput
+                    ></TextInput>
+                </View>
+                <View style={styles.form_row}>
+                    <TextInput
+                        style={styles.input_ML}
+                        onChangeText={this.heroChange("invt")}
+                        multiline={true}
+                        numberOfLines={10}
+                    >
+                        {this.state.invt}
+                    </TextInput>
+
+                </View>
+                <Pressable
+                    onPress={() => this.createTwoButtonAlert(navigation)}
+                    style={styles.button}
+                >
+                    <Text style={styles.text}>Delete Character</Text>
+                </Pressable>
+                <Pressable
+                    onPress={() => this.loadData(this.state.key)}
+                    style={styles.button}
+                >
+                    <Text style={styles.text}>Reset Form</Text>
+                </Pressable>
+                </ScrollView>
+                <View style={styles.but_row}>
+                {/* unincorporated buttons */}
+                <Pressable
+                    onPress={() => {this.setData(navigation)}}
+                    style={styles.foot_btn}
+                >
+                    <Text style={styles.text}>Save</Text>
+                </Pressable>
+                <Pressable
+                    onPress={() => {navigation.goBack()}}
+                    style={styles.foot_btn}
+                >
+                    <Text style={styles.text}>Cancel</Text>
+                </Pressable>
+            </View>
+            </SafeAreaView>
+        );
+    };
 }
 
-//Displays alert on the user screen
-function createTwoButtonAlert(key, navigation) {
-  Alert.alert(
-    "Warning!",
-    "You are about to delete this character. Do you want to continue with this deletion?",
-    [
-      //Cancels deletion
-      {
-        text: "No",
-        onPress: () => console.log("Cancel Pressed"),
-        style: "cancel"
-      },
-      //Deletes data
-      { text: "Yes", 
-        onPress:() => deleteHandle(key, navigation) }
-    ]
-  );
-}
-
-function Increment(item, setItem){
-  item.attk = String(parseInt(item.attk) + 1)
-  setItem(item)
-}
-
-//Screen to be rendered
-export default function EditScreen({ route }) {
-  const [item, setItem] = useState({})
-  const [attkText, setAttkText] = useState()
-  const [defnText, setDefnText] = useState()
-  const [bodyText, setBodyText] = useState()
-  const [mindText, setMindText] = useState()
-  const [gold, setGold] = useState()
-
-  const navigation = useNavigation();
-  const key = route.params.key
-
-  useEffect(() => {
-    getHandle(key, setItem, setAttkText, setDefnText, setBodyText, setMindText)
-  }, [])
-
-  return (
-    // Placeholder screen
-    <SafeAreaView>
-      <ScrollView>
-        <TextInput style={styles.header}>{item.name}</TextInput>
-        <TextInput style={styles.subheader}>{item.clss}</TextInput>
-    
-        <View style={styles.row}>
-            <View style={styles.box}>
-                <Text>Attack</Text>
-                <Button
-                  onPress={() => {
-                    temp = item
-                    temp.attk = String(parseInt(temp.attk) + 1)
-                    setItem(temp)
-                    setAttkText(temp.attk)
-                  }}
-                  title="+"
-                />
-                <TextInput
-                  
-                >{item.attk}</TextInput>
-                <Button
-                  onPress={() => {
-                    temp = item
-                    if(temp.attk > 1){
-                    temp.attk = String(parseInt(temp.attk) - 1)
-                    }
-                    setItem(temp)
-                    setAttkText(temp.attk)
-                  }}
-                  title="-"
-                />
-
-            </View>
-            <View style={styles.box}>
-                <Text>Defense</Text>
-                <Button
-                  onPress={() => {
-                    temp = item
-                    temp.defn = String(parseInt(temp.defn) + 1)
-                    setItem(temp)
-                    setDefnText(temp.defn)
-                  }}
-                  title="+"
-                />
-                <TextInput
-                  
-                >{item.defn}</TextInput>
-                <Button
-                  onPress={() => {
-                    temp = item
-                    if(temp.defn > 2){
-                    temp.defn = String(parseInt(temp.defn) - 1)
-                    }
-                    setItem(temp)
-                    setAttkText(temp.defn)
-                  }}
-                  title="-"
-                />
-            </View>
-            <View style={styles.box}>
-                <Text>Body</Text>
-                <Button
-                  onPress={() => {
-                    temp = item
-                    temp.body = String(parseInt(temp.body) + 1)
-                    setItem(temp)
-                    setAttkText(temp.body)
-                  }}
-                  title="+"
-                />
-                <TextInput
-                  
-                >{item.body}</TextInput>
-                <Button
-                  onPress={() => {
-                    temp = item
-                    if(temp.body > 0){
-                    temp.body = String(parseInt(temp.body) - 1)
-                    }
-                    setItem(temp)
-                    setAttkText(temp.body)
-                  }}
-                  title="-"
-                />
-            </View>
-            <View style={styles.box}>
-                <Text>Mind</Text>
-                <Button
-                  onPress={() => {
-                    temp = item
-                    temp.mind = String(parseInt(temp.mind) + 1)
-                    setItem(temp)
-                    setAttkText(temp.mind)
-                  }}
-                  title="+"
-                />
-                <TextInput
-                  
-                >{item.mind}</TextInput>
-                <Button
-                  onPress={() => {
-                    temp = item
-                    if(temp.mind > 0){
-                    temp.mind = String(parseInt(temp.mind) - 1)
-                    }
-                    setItem(temp)
-                    setAttkText(temp.mind)
-                  }}
-                  title="-"
-                />
-            </View>
-        </View>
-        <View style={styles.form_row}>
-            <Text style={styles.label}>Gold</Text>
-            <TextInput
-                style={styles.input}
-                onChangeText={setGold}
-                placeholder = {item.gold}
-                keyboardType='numeric'
-            />
-        </View>
-        {/* updates hero inventory */}
-        <View style={styles.form_row}>
-            <Text style={styles.subheader}>Inventory</Text>
-            <TextInput></TextInput>
-        </View>
-        {/* <View style={styles.form_row}>
-            <TextInput
-                style={styles.input_ML}
-                onChangeText={this.heroChange("invt")}
-                multiline={true}
-                numberOfLines={10}
-            />
-        </View> */}
-        <Text>Blah</Text>
-        <Pressable
-          onPress={() => createTwoButtonAlert(key, navigation)}
-          style={styles.button}
-        >
-          <Text style={styles.text}>Delete Character</Text>
-        </Pressable>
-      </ScrollView>
-      <View style={styles.but_row}>
-        {/* unincorporated buttons */}
-        <Pressable
-            onPress={() => {saveHandle(key, item, navigation)}}
-            style={styles.foot_btn}
-        >
-            <Text style={styles.text}>Save</Text>
-        </Pressable>
-        <Pressable
-            onPress={() => {navigation.goBack()}}
-            style={styles.foot_btn}
-        >
-            <Text style={styles.text}>Cancel</Text>
-        </Pressable>
-    </View>
-    </SafeAreaView>
-  );
-}
-
-
+export default EditScreen;
